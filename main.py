4fbc8e5a46403ac0c2c466d0aa9dbc512647da21
MariaXavier7 <mariajuliaxavierdasilva5@gmail>
2026-07-28 17:57:41 -0300

    2807

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import os
 
-from flask import  render_template, Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     nome = "icoma.com"
-    return render_template('index.html', site = nome)
+    return render_template("index.html", site=nome)
 
-@app.router("/login")
+
+@app.route("/login")
 def login():
-    return render_template('login/login.html')
+    return render_template("login/login.html")
+
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+    app.run(
+        port=int(os.environ.get("PORT", 80)),
+        debug=True
+    )
+
 
 if __name__ == "__main__":
     main()

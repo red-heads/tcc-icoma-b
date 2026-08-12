66fc70ac0f65f70d80efb8f62ee8acd0a8a4ac80
Karollayne Faggionato <160078288+KarolFaggionato@users.noreply.github.com>
2026-08-12 17:29:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,20 @@
 import os
 
-from flask import Flask, render_template
+from flask import render_template, Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    nome = "icoma.com"
-    return render_template("index.html", site=nome)
-
+    nome = 'icoma.com.br'
+    return render_template('index.html', site = nome)
 
 @app.route("/login")
 def login():
-    return render_template("login/login.html")
-
-@app.route("/respiracao")
-def respiracao():
-    return render_template("pages/respiracao.html")
-
-
-@app.route("/pausa")
-def pausa():
-    return render_template("pages/pausa.html")
+    return render_template('login/login.html')
 
 def main():
     app.run(host="0.0.0.0", port = int(os.environ.get("PORT", 10000)))
-
-
-if __name__ == "__main__":
-    main()
-    
-def main():
-    app.run(
-        port=int(os.environ.get("PORT", 80)),
-        debug=True
-    )
-
 
 if __name__ == "__main__":
     main()
